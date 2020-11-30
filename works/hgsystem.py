692c0430fb2746c3398d9a4ab554dd19785cfde2
Augustus Chen <augustuschen@nexcom.com.tw>
2020-11-30 16:13:10 +0800

    2020/11/30-2

--- works/hgsystem.py
+++ works/hgsystem.py
@@ -5,7 +5,7 @@ from PySide2.QtGui import QFont # pylint: disable=no-name-in-module
 ###################################################################################################
 VER_MAJOR = 0
 VER_MINOR = 3
-VER_PATCH = 0
+VER_PATCH = "wtf"
 VER_STRING = f"{VER_MAJOR}.{VER_MINOR}.{VER_PATCH}"
 
 """History
