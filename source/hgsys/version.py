84a4bf49760c431f8d0ff0ba6fd2a25b24cf49be
Augustus Chen <augustus.sanchung@gmail.com>
2026-05-12 09:21:17 +0800

    將版本改成 0.7.0

--- source/hgsys/version.py
+++ source/hgsys/version.py
@@ -1,11 +1,1 @@
-VER_MAJOR: int = 0
-VER_MINOR: int = 6
-VER_PATCH: int = 0
-VER_EXTRA: str = ""
-VER_STRING: str = f"{VER_MAJOR}.{VER_MINOR}.{VER_PATCH}{VER_EXTRA}"
-
-"""History
-0.6.0:
-    Rewrite as a Python package with MVVM separation
-    (domain / repository / services / viewmodels / views).
-"""
+VER_STRING: str = "0.7.0"
